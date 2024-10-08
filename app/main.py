995d0a0ab5ffc4b91b305d2cca4f877ddd65ec48
Matheus Gentil <matheusvgentil@hotmail.com>
2024-10-08 18:00:48 -0300

    add mkdocs

--- app/main.py
+++ app/main.py
@@ -7,7 +7,5 @@ if __name__ == '__main__':
     path = 'data/input'
 
     lista_dataframes = extract_data_from_xlsx(path)
-    print(type(lista_dataframes))
     dataframe = concat_dataframes(lista_dataframes)
-    print(type(dataframe))
     load_xlsx(dataframe, 'data/output', 'output')
